abdfe97401c65d5af04ea97d2afb022beb9e6988
Vincent Moens <Vincent.Moens@aig.com>
2018-11-13 15:57:59 +0000

    fc correc

--- src/optim.jl
+++ src/optim.jl
@@ -6,7 +6,7 @@ mutable struct optimize
 		return new([],optimizer)
 	end
 end
-function (OPT::optimize)(F::FIVOChain,RT,C,X;gradient_fetch_interval::Integer=200,continuous_opt::Bool=true)
+function (OPT::optimize)(F::FIVOChain,RT,C,X;gradient_fetch_interval::Integer=200,continuous_opt::Bool=false)
 	opt = OPT.optimizer
 	if continuous_opt
 		opt_local = ()->begin
@@ -17,14 +17,14 @@ function (OPT::optimize)(F::FIVOChain,RT,C,X;gradient_fetch_interval::Integer=20
 		opt_local = ()->nothing
 	end
 
-	OPT.fc_out = []
-	for t in 1:1000
+	for t in 1:10000
 		ss = rand(1:length(RT))
 		L = -F(RT[ss],C[ss],X[ss],gradient_fetch_interval=gradient_fetch_interval,opt_local=opt_local)
 		Tracker.back!(L)
 		opt()
 		zero_grad!(F)
 		if t % 10 == 0 || t == 1
+			OPT.fc_out = []
 			for ss in 1:length(RT)
 				push!(OPT.fc_out, F(RT[ss],C[ss],X[ss],eval=true))
 			end
