a6783e164c5210f427948aecafd541bfbc2fa7aa
Vincent Moens <Vincent.Moens@aig.com>
2019-06-04 06:36:02 +0100

    Cuda

--- src/RaeDm.jl
+++ src/RaeDm.jl
@@ -1,23 +1,15 @@
 module RaeDm
 
-macro mainDef(x)
-    """
-        Variables defined using @mainDef macro are overwritten in module if they exist in the Main environment.
-        When this occurs, @mainDef warns the user about this.
-    """
-        xn = string(x.args[1])
-        y = isdefined(Main,x.args[1]) ? :(Printf.@printf("Warning: Overwriting %s to %s\n",$xn,Main.$(x.args[1]));const $(x.args[1]) = Main.$(x.args[1])) : Expr(:const,x)
-        esc(y)
-end
 
-@mainDef isCu = (Base.find_package("CuArrays") != nothing)
-if isCu
+isCu = Ref[false]
+CUDA(set=true) = (isCu[] = set)
+if isCu[]
     print("Using CUDA\n")
     try
     	using CuArrays
     catch
  	@warn "failed to load cuda"
-	isCu=false
+	isCu[]=false
     end
 end
 using Flux, ForwardDiff, DiffResults, StatsFuns, NNlib, Statistics
